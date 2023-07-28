18c62f76cf56102fe6a2f3a6238d5521668217d1
mwovi <petermwovi73@gmail.com>
2023-07-28 14:18:39 +0300

    Edit date.js file to implement linters suggestions

--- modules/date.js
+++ modules/date.js
@@ -1,7 +1,5 @@
 import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
-const formatDate = () => {
-  return DateTime.now().toLocaleString(DateTime.DATETIME_MED);
-};
+const formatDate = () => DateTime.now().toLocaleString(DateTime.DATETIME_MED);
 
 export default formatDate;
