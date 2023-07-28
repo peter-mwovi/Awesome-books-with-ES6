ec4289c20f9bbe6a9e0461058cf2ccd0907064fc
mwovi <petermwovi73@gmail.com>
2023-07-28 14:12:20 +0300

    Edit index.js to implement linters suggestions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { UI } from './modules/ui.js';
-import { Book } from './modules/book.js';
-import { formatDate } from './modules/date.js';
+import UI from './modules/ui.js';
+import Book from './modules/book.js';
+import formatDate from './modules/date.js';
 
 // Event listeners
 document.getElementById('bookForm').addEventListener('submit', (e) => {
@@ -46,51 +46,9 @@ window.showContent = (itemId) => {
   }
 };
 
-// Get the date and format it as 'March 19th 2021 9:25:32 am'
-//  function getCurrentFormattedDate () {
-//   const months = [
-//     'January',
-//     'February',
-//     'March',
-//     'April',
-//     'May',
-//     'June',
-//     'July',
-//     'August',
-//     'September',
-//     'October',
-//     'November',
-//     'December',
-//   ];
-//   const today = new Date();
-//   const month = months[today.getMonth()];
-//   const day = String(today.getDate());
-//   const year = today.getFullYear();
-//   let hours = today.getHours();
-//   const minutes = String(today.getMinutes()).padStart(2, '0');
-//   const seconds = String(today.getSeconds()).padStart(2, '0');
-//   const ampm = hours >= 12 ? 'pm' : 'am';
-
-//   // Convert to 12-hour format
-//   hours = hours % 12 || 12;
-
-//   // Add 'st', 'nd', 'rd', or 'th' to the day based on its value
-//   let daySuffix = 'th';
-//   if (day === '1' || day === '21' || day === '31') {
-//     daySuffix = 'st';
-//   } else if (day === '2' || day === '22') {
-//     daySuffix = 'nd';
-//   } else if (day === '3' || day === '23') {
-//     daySuffix = 'rd';
-//   }
-
-//   return `${month} ${day}${daySuffix} ${year} ${hours}:${minutes}:${seconds} ${ampm}`;
-// }
-
 // Update the innerHTML of the dateDiv with the formatted date
 document.addEventListener('DOMContentLoaded', () => {
   const dateDiv = document.getElementById('date');
   dateDiv.innerHTML = formatDate();
 });
 
-showContent('book');
